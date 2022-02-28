ef47d2df69802752947545451d12f10aa8d8fea1
Youness Dradeb <youness.dradeb@gmail.com>
2022-02-28 17:00:08 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,33 @@
 $(document).ready(function () {
+
+
+    var arab = $("#languageindic").html == "FR" ? true : false ;
+    // alert(arab)
+    if(arab)
+    {
+        $("#languageindic a").html("AR");
+        $("#fr").html("AR/FR");
+    }else{
+        $("#languageindic a").html("FR");
+        $("#fr").html("AR/FR");
+    }
+    $("#fr").click(function(){
+        if(arab)
+        {
+            setLanguage('fr'); 
+            $("body").removeClass("ar");
+            $("#mainmenu").removeClass("ar");
+            $("#fr").html("AR/FR");
+            arab = false ;
+        }else{
+            setLanguage('ar');
+            $("body").addClass("ar");
+            $("#mainmenu .nav-item a").addClass("ar");
+            $("#fr").html("AR/FR");
+            arab = true ;
+        }
+       
+    });
     $("#closem").hide();
     $("#closem").click(function() {
         $("#mainmenu").removeClass("active");
